1b91ce1e49e4be3779229dfe579aab4ea5ab388c
ShiLei Cui <scui5@binghamton.edu>
2018-05-29 23:25:57 -0400

    set database to heroku

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'shileicui',
-    password : '',
-    database : 'face-recognition'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
