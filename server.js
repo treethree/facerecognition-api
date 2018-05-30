a65aadd796bfdbe3c6d80ec1e0b756fb0fe63a10
ShiLei Cui <scui5@binghamton.edu>
2018-05-29 23:05:33 -0400

    adding port

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, () => {
-  console.log('app is running');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port 3000 ${process.env.PORT}`);
 })
